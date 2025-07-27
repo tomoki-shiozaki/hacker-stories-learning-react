7dd5dd3a8f3256e4f104a66e86d9b6671544e0c8
shiozaki <shiozaki.tomoki2@gmail.com>
2025-07-27 13:28:26 +0900

    feat: create hierarchy of React components

--- src/App.js
+++ src/App.js
@@ -23,24 +23,40 @@ function App() {
   return (
     <div>
       <h1>My Hacker Stories</h1>
+      <Search />
+
+      <hr />
+
+      <List />
+    </div>
+  );
+}
+
+function Search() {
+  return (
+    <div>
       <label htmlFor="search">Search: </label>
       <input id="search" type="text" />
-      <hr />
-      <ul>
-        {list.map(function (item) {
-          return (
-            <li key={item.objectID}>
-              <span>
-                <a href={item.url}>{item.title}</a>
-              </span>
-              <span>{item.author}</span>
-              <span>{item.num_comments}</span>
-              <span>{item.points}</span>
-            </li>
-          );
-        })}
-      </ul>
     </div>
+  );
+}
+
+function List() {
+  return (
+    <ul>
+      {list.map(function (item) {
+        return (
+          <li key={item.objectID}>
+            <span>
+              <a href={item.url}>{item.title}</a>
+            </span>
+            <span>{item.author}</span>
+            <span>{item.num_comments}</span>
+            <span>{item.points}</span>
+          </li>
+        );
+      })}
+    </ul>
   );
 }
 
