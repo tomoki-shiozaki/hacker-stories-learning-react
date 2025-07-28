41efd4ca35233a4fe300a3fb5ed1bfd5b4207dc5
shiozaki <shiozaki.tomoki2@gmail.com>
2025-07-28 11:10:59 +0900

    feat: learn callback function

--- src/App.js
+++ src/App.js
@@ -19,20 +19,26 @@ const App = () => {
       objectID: 1,
     },
   ];
+
+  const handleSearch = (event) => {
+    console.log(event.target.value);
+  };
+
   return (
     <div>
       <h1>My Hacker Stories</h1>
-      <Search />
+      <Search onSearch={handleSearch} />
       <hr />
       <List list={stories} />
     </div>
   );
 };
 
-const Search = () => {
+const Search = (props) => {
   const [searchTerm, setSearchTerm] = React.useState("");
   const handleChange = (event) => {
     setSearchTerm(event.target.value);
+    props.onSearch(event);
   };
   return (
     <div>
