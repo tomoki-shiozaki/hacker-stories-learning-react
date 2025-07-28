641959cf57ceb30afd5afe6cdb3e1633260aa9b3
shiozaki <shiozaki.tomoki2@gmail.com>
2025-07-28 12:35:46 +0900

    feat: Props Handling (Advanced)

--- src/App.js
+++ src/App.js
@@ -40,39 +40,31 @@ const App = () => {
   );
 };
 
-const Search = (props) => {
-  return (
-    <div>
-      <label htmlFor="search">Search: </label>
-      <input
-        id="search"
-        type="text"
-        value={props.search}
-        onChange={props.onSearch}
-      />
-      <p>
-        Searching for <strong>{props.search}</strong>.
-      </p>
-    </div>
-  );
-};
+const Search = ({ search, onSearch }) => (
+  <div>
+    <label htmlFor="search">Search: </label>
+    <input id="search" type="text" value={search} onChange={onSearch} />
+    <p>
+      Searching for <strong>{props.search}</strong>.
+    </p>
+  </div>
+);
 
-const List = (props) => (
+const List = ({ list }) => (
   <ul>
-    {props.list.map((item) => (
+    {list.map((item) => (
       <Item key={item.objectID} item={item} />
     ))}
   </ul>
 );
-
-const Item = (props) => (
+const Item = ({ item }) => (
   <li>
     <span>
-      <a href={props.item.url}>{props.item.title}</a>
+      <a href={item.url}>{item.title}</a>
     </span>
-    <span>{props.item.author}</span>
-    <span>{props.item.num_comments}</span>
-    <span>{props.item.points}</span>
+    <span>{item.author}</span>
+    <span>{item.num_comments}</span>
+    <span>{item.points}</span>
   </li>
 );
 
