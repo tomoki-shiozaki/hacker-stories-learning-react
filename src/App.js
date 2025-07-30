7d66351056040277c5aba21055ca1abed90edf7c
shiozaki <shiozaki.tomoki2@gmail.com>
2025-07-30 18:51:37 +0900

    feat: learn Reusable React Component

--- src/App.js
+++ src/App.js
@@ -43,20 +43,27 @@ const App = () => {
   return (
     <div>
       <h1>My Hacker Stories</h1>
-      <Search search={searchTerm} onSearch={handleSearch} />
+      <InputWithLabel
+        id="search"
+        label="Search"
+        value={searchTerm}
+        onInputChange={handleSearch}
+      />
+      <p>
+        Searching for <strong>{searchTerm}</strong>.
+      </p>
+
       <hr />
       <List list={searchedStories} />
     </div>
   );
 };
 
-const Search = ({ search, onSearch }) => (
+const InputWithLabel = ({ id, label, value, type = "text", onInputChange }) => (
   <>
-    <label htmlFor="search">Search: </label>
-    <input id="search" type="text" value={search} onChange={onSearch} />
-    <p>
-      Searching for <strong>{search}</strong>.
-    </p>
+    <label htmlFor={id}>{label}</label>
+    &nbsp;
+    <input id={id} type={type} value={value} onChange={onInputChange} />
   </>
 );
 
