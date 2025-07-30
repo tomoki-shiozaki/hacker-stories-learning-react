8c6cd5279abcffd15041716c6ab96ff498e13778
shiozaki <shiozaki.tomoki2@gmail.com>
2025-07-30 18:38:05 +0900

    feat: learn React Fragments

--- src/App.js
+++ src/App.js
@@ -51,13 +51,13 @@ const App = () => {
 };
 
 const Search = ({ search, onSearch }) => (
-  <div>
+  <>
     <label htmlFor="search">Search: </label>
     <input id="search" type="text" value={search} onChange={onSearch} />
     <p>
       Searching for <strong>{search}</strong>.
     </p>
-  </div>
+  </>
 );
 
 const List = ({ list }) => (
