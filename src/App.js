cbb95c0aa4f4dc0deba2d06bb80c425feec1f6ab
shiozaki <shiozaki.tomoki2@gmail.com>
2025-07-31 18:32:36 +0900

    feat: Imperative React (as imperative form)

--- src/App.js
+++ src/App.js
@@ -68,19 +68,31 @@ const InputWithLabel = ({
   onInputChange,
   isFocused,
   children,
-}) => (
-  <>
-    <label htmlFor={id}>{children}</label>
-    &nbsp;
-    <input
-      id={id}
-      type={type}
-      value={value}
-      autoFocus={isFocused}
-      onChange={onInputChange}
-    />
-  </>
-);
+}) => {
+  // A
+  const inputRef = React.useRef();
+  // C
+  React.useEffect(() => {
+    if (isFocused && inputRef.current) {
+      // D
+      inputRef.current.focus();
+    }
+  }, [isFocused]);
+  return (
+    <>
+      <label htmlFor={id}>{children}</label>
+      &nbsp;
+      {/* B */}
+      <input
+        ref={inputRef}
+        id={id}
+        type={type}
+        value={value}
+        onChange={onInputChange}
+      />
+    </>
+  );
+};
 
 const List = ({ list }) => (
   <ul>
