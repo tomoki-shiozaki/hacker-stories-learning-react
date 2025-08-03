6e8ea70b6c987830bbd010884f38bfde4413431f
shiozaki <shiozaki.tomoki2@gmail.com>
2025-08-03 15:14:25 +0900

    feat: learn Data Fetching with React

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import List from "./components/List";
 import { getAsyncStories } from "./api/getStories";
 import storiesReducer from "./reducers/storiesReducer";
 
+const API_ENDPOINT = "https://hn.algolia.com/api/v1/search?query=";
+
 const App = () => {
   const [searchTerm, setSearchTerm] = useSemiPersistentState("search", "React");
   const [stories, dispatchStories] = React.useReducer(storiesReducer, {
@@ -16,11 +18,12 @@ const App = () => {
   React.useEffect(() => {
     dispatchStories({ type: "STORIES_FETCH_INIT" });
 
-    getAsyncStories()
+    fetch(`${API_ENDPOINT}react`) // B
+      .then((response) => response.json()) // C
       .then((result) => {
         dispatchStories({
           type: "STORIES_FETCH_SUCCESS",
-          payload: result.data.stories,
+          payload: result.hits, // D
         });
       })
       .catch(() => dispatchStories({ type: "STORIES_FETCH_FAILURE" }));
