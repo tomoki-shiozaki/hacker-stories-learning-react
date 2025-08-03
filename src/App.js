49c6e7d3b860fdb52bb2992e0a0a3fe027385feb
shiozaki <shiozaki.tomoki2@gmail.com>
2025-08-03 18:49:17 +0900

    feat: learn Async/Await in React (Advanced)

--- src/App.js
+++ src/App.js
@@ -19,18 +19,18 @@ const App = () => {
     isError: false,
   });
 
-  const handleFetchStories = useCallback(async () => {
+  const handleFetchStories = React.useCallback(async () => {
     dispatchStories({ type: "STORIES_FETCH_INIT" });
 
-    axios
-      .get(url)
-      .then((result) => {
-        dispatchStories({
-          type: "STORIES_FETCH_SUCCESS",
-          payload: result.data.hits,
-        });
-      })
-      .catch(() => dispatchStories({ type: "STORIES_FETCH_FAILURE" }));
+    try {
+      const result = await axios.get(url);
+      dispatchStories({
+        type: "STORIES_FETCH_SUCCESS",
+        payload: result.data.hits,
+      });
+    } catch {
+      dispatchStories({ type: "STORIES_FETCH_FAILURE" });
+    }
   }, [url]);
 
   React.useEffect(() => {
