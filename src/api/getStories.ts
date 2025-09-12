308b322f8b1b95c481e0f8bd1164a65cfae68e83
shiozaki <shiozaki.tomoki2@gmail.com>
2025-09-12 12:29:26 +0900

    refactor(API): add types for getAsyncStories function

--- src/api/getStories.ts
+++ src/api/getStories.ts
@@ -1,10 +1,17 @@
+import { Story } from "../types";
+
 const API_ENDPOINT = "https://hn.algolia.com/api/v1/search?query=";
 
-export const getAsyncStories = async (query) => {
+type AlgoliaResponse = {
+  hits: Story[];
+};
+
+export const getAsyncStories = async (query: string): Promise<Story[]> => {
   const response = await fetch(`${API_ENDPOINT}${query}`);
   if (!response.ok) {
     throw new Error("Fetch failed");
   }
-  const result = await response.json();
+
+  const result: AlgoliaResponse = await response.json();
   return result.hits;
 };
