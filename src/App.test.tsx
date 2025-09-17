b6b9a7cc9d3dbf1b1dd08ae6f92de10014f2c190
shiozaki <shiozaki.tomoki2@gmail.com>
2025-09-17 15:17:21 +0900

    test(reducer): add tests for storiesReducer

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,8 +1,17 @@
-import { render, screen } from "@testing-library/react";
-import App from "./App";
-
-test("renders learn react link", () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("something truthy and falsy", () => {
+  test("true to be true", () => {
+    expect(true).toBe(true);
+  });
+  test("false to be false", () => {
+    expect(false).toBe(false);
+  });
 });
+
+test("true to be true", () => {
+  expect(true).toBe(true);
+});
+test("false to be false", () => {
+  expect(false).toBe(false);
+});
+
+export {};
