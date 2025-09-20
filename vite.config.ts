485ce40422bd7fe4bf66400df6313c6f430fe58e
shiozaki <shiozaki.tomoki2@gmail.com>
2025-09-20 13:53:23 +0900

    chore(coverage): add coveage-v8 setting

--- vite.config.ts
+++ vite.config.ts
@@ -9,5 +9,16 @@ export default defineConfig({
     globals: true,
     environment: "jsdom",
     setupFiles: "./src/test/setup.ts",
+    coverage: {
+      provider: "v8",
+      reporter: ["text", "html", "lcov"],
+      exclude: [
+        "vite.config.ts", // Vite 設定
+        "eslint.config.js", // ESLint 設定
+        "src/vite-env.d.ts", // 型定義ファイル
+        "src/main.tsx", // エントリーポイント除外
+        "src/types/**", // 型定義除外
+      ],
+    },
   },
 });
